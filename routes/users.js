b422280852d082fa3803c7f8855f16d964e1cb3c
viperist <somnathc276@gmail.com>
2024-04-28 21:41:29 +0530

    fixed mongoose connection

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb+srv://somnathc276:123456789@@pinterest.vjjudfk.mongodb.net/?retryWrites=true&w=majority&appName=Pinterest");
+mongoose
+  .connect(
+    "mongodb+srv://somnathc276:vNxvpWU5zmUsBN7x@pinterest.vjjudfk.mongodb.net/?retryWrites=true&w=majority&appName=Pinterest"
+  )
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log("Mongo error:", err));
 
 const userSchema = mongoose.Schema({
-  username:String,
-  name:String,
+  username: String,
+  name: String,
   email: String,
   password: String,
   profileImage: String,
   contact: Number,
   boards: {
     type: Array,
-    default: []
+    default: [],
   },
-  posts:[
+  posts: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "post"
-    }
-  ]
+      ref: "post",
+    },
+  ],
 });
 
 userSchema.plugin(plm);
 
-module.exports = mongoose.model("user",userSchema);
+module.exports = mongoose.model("user", userSchema);
